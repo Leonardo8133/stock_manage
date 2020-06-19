3a082b02426a82b243efe13e56ca9859bd8475ba
Leonardo8133 <62627215+Leonardo8133@users.noreply.github.com>
2020-06-18 21:35:52 -0300

    Site Upload

---
+++ static/git/javascript.js
@@ -0,0 +1,42 @@
+var current = 1;
+var paused = true;
+
+
+function slide_update() {
+	var images = document.getElementsByClassName('slideimg');
+	if (paused) {
+
+	}
+	else
+	{
+	for (var i = 0; i < images.length; i++) {
+		images[i].style.display = 'none';
+		images[i].classList.remove = 'showing';
+
+	}
+	images[current].style.display = "list-item";
+	images[current].classList.toggle = 'showing';
+	current++;
+	if (current >= images.length) {current = 0};
+	
+	}
+	setTimeout(slide_update, 4000);
+}
+
+function switch_pause(){
+	paused = !paused;
+	document.getElementById('button').classList.toggle('pressed');
+
+}
+
+function full(th) {
+	ab = document.getElementsByTagName("ul")[0]
+	console.log(ab)
+	ab.classList.toggle("full");
+	if (paused) {
+
+	}
+	else {
+		switch_pause()
+	}
+}
